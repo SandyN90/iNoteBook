5ae44d232f071e677ca7b9f45555c0674b13f1d8
sandy <sandeepkumarnavik5@gmail.com>
2023-05-23 08:51:19 +0530

    Added react-icons in project

--- iNotebook/src/components/Navbar.jsx
+++ iNotebook/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./../index.css"
+import {HiPhotograph} from 'react-icons/all';
 
 export default function Navbar() {
     const navElements = [
@@ -11,10 +12,10 @@ export default function Navbar() {
       ]
     return (
       <div className="flex justify-between items-center ml-5">
-        <div>logo</div>
+        <div><HiPhotograph className="h-6 w-6"/></div>
         <div className="flex items-center justify-around mr-5">
-        <ul className="flex gap-8 p-4 justify-end mr-5">{navElements.map((element)=> <li className="text-blue-500">{element.tabName}</li>)}</ul>
-        <div>Profile logo</div>
+        <ul className="flex gap-8 p-4 justify-end mr-5">{navElements.map((element)=> <li className="text-blue-500" key={element.tabName}>{element.tabName}</li>)}</ul>
+        <HiPhotograph className="h-10 w-10"/>
         </div>
       </div>
 
